a1cbb5bf5fc0fb934864895abdddeffa1c5491c9
Stenber Wyatte <Stenber.Wyatte@gmail.com>
2019-02-27 18:52:11 -0800

    Some initial file creation

---
+++ Stenber.Desktop/Stenber.Desktop/Interfaces/IDesktopManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stenber.Desktop.Interfaces
+{
+    public interface IDesktopManager
+    {
+        bool IsDesktopFormCovered { get; }
+
+        void Reset();
+    }
+}
